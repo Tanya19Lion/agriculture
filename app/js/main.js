ebe7b2b5c3d125c3acfead29fb155aef627f5202
Tanya19Lion <tanyatriteknitskaya@gmail.com>
2020-03-11 17:33:40 +0200

    added header_menu and made adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -4,9 +4,26 @@ $(function(){
         slidesToShow: 2,
         slidesToScroll: 2,
         arrows: false,
-        dots: true
+        dots: true,
+        responsive: [
+            {
+              breakpoint: 1021,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+            }
+        ]
     });
 
+    $(".header_inner_menu").on("click", function(){
+        $(".header_menu").addClass("active");
+    });
+    $(".close_btn").on("click", function(){
+        $(".header_menu").removeClass("active");
+    });
+
+    new WOW().init();
 
 
 });
